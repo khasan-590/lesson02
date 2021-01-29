7b0d8dc1f75ed005ce2a9b65192cdfe868ab1de4
khasan-590 <khasan4ik590@gmail.com>
2021-01-30 02:43:32 +0600

    first commit

---
+++ Js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+let  money = "1000";  //“Доход за месяц”
+console.log(typeof money);
+let  income = 'Курьер'; // Дополнительный доход.
+console.log(typeof income);
+let addExpenses ="Такси, Проживание, Питание, Налоги";
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+let  deposit = true; //любое булево значение,
+console.log(typeof deposit);
+let  mission = 50000; //любое число(Какую сумму хотите накопить)
+
+let  period = 6; //число от 1 до 12(месяцев)
+console.log( mission + ' доллар, за ' + period  + ' месяцев');
+
+let budgetDay = money / 30;
+  console.log(budgetDay + " $");
+
+
